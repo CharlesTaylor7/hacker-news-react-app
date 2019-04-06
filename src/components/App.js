96f81059098b704095154b82d9029d9a221ad1ce
Charles Taylor <charlestaylor95@gmail.com>
2019-04-05 20:45:10 -0400

    Convert folder to story

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Folder from './Folder';
+import Story from './Story';
 
 export default () => {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Folder folder={{ name: 'Project', id: 'root' }} />
+        <Story story={{ title: 'An Article' }} />
       </header>
     </div>
   );
